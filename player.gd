893820088861cc25a54f36ec5e51bf2b99043603
unerringCaprition <penumbruh@gmail.com>
2026-01-01 15:19:16 -0800

    ohhh the sckrinkly dinkly!
    ohhh the scrungy wungy!
    we can move the little godot placeholder image around on the screen and it is very silly. oooh yayyy.
    i went for input that would feel decently okay with moving around. i think. no diagonals. i think that might be something you can unlock though? we'll see.
    also i scrunched down the size of things because i want this to be game boy color like

--- player.gd
+++ player.gd
@@ -1,22 +1,61 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
+const SPEED = 50.0
 const JUMP_VELOCITY = -400.0
 
+var move_dir = "none"
 
 func _physics_process(delta: float) -> void:
 	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction_x := Input.get_axis("left", "right")
-	if direction_x:
-		velocity.x = direction_x * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		var direction_y := Input.get_axis("up", "down")
-		if direction_y:
-			velocity.y = direction_y * SPEED
+	
+	if Input.is_action_just_pressed("down"):
+		move_dir = "down"
+	if Input.is_action_just_pressed("up"):
+		move_dir = "up"
+	if Input.is_action_just_pressed("left"):
+		move_dir = "left"
+	if Input.is_action_just_pressed("right"):
+		move_dir = "right"
+	
+	if Input.is_action_just_released(move_dir):
+		if Input.is_action_pressed("down"):
+			move_dir = "down"
+		elif Input.is_action_pressed("up"):
+			move_dir = "up"
+		elif Input.is_action_pressed("left"):
+			move_dir = "left"
+		elif Input.is_action_pressed("right"):
+			move_dir = "right"
 		else:
-			velocity.y = move_toward(velocity.y, 0, SPEED)
+			move_dir = "none"
+	
+	var direction: Vector2
+	match move_dir:
+		"down":
+			direction = Vector2(0.0, 1.0)
+		"up":
+			direction = Vector2(0.0, -1.0)
+		"right":
+			direction = Vector2(1.0, 0.0)
+		"left":
+			direction = Vector2(-1.0, 0.0)
+		"none":
+			direction = Vector2(0.0, 0.0)
+	
+	velocity = direction * SPEED
+	
+	#var direction_x := Input.get_axis("left", "right")
+	#var direction_y := Input.get_axis("up", "down")
+	#if direction_x:
+		#velocity.x = direction_x * SPEED
+	#else:
+		#velocity.x = move_toward(velocity.x, 0, SPEED)
+	#
+	#if direction_y and !direction_x:
+		#velocity.y = direction_y * SPEED
+	#else:
+		#velocity.y = move_toward(velocity.y, 0, SPEED)
+
 
 	move_and_slide()
