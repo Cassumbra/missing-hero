2c3e2347d47baa108f866a70537f0b6f1f62fca1
unerringCaprition <penumbruh@gmail.com>
2026-01-01 23:20:27 -0800

    player sprite. axe stuff
    actually use the player sprite we made
    also, we're making decent progress on implementing axe swinging.

---
+++ axe_handler.gd
@@ -0,0 +1,25 @@
+extends Node2D
+
+@onready var swinging_timer: Timer = $SwingingTimer
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_player_swing_axe(direction: String) -> void:
+	swinging_timer.start()
+	
+	match direction:
+		"down":
+			rotation_degrees = 0.0
+		"up":
+			rotation_degrees = 180.0
+		"left":
+			rotation_degrees = 90.0
+		"right":
+			rotation_degrees = 270.0
--- player.gd
+++ player.gd
@@ -1,34 +1,46 @@
 extends CharacterBody2D
 
+@onready var swinging_timer = $AxeHandler/SwingingTimer
+
+signal swing_axe(direction: String)
 
 const SPEED = 50.0
-const JUMP_VELOCITY = -400.0
 
 var move_dir = "none"
+var face_dir = "down"
+
+var timer_just_stopped = false
 
 func _physics_process(delta: float) -> void:
-	# Get the input direction and handle the movement/deceleration.
+	if Input.is_action_just_pressed("button a"):
+		swing_axe.emit(face_dir)
+		move_dir = "none"
 	
-	if Input.is_action_just_pressed("down"):
-		move_dir = "down"
-	if Input.is_action_just_pressed("up"):
-		move_dir = "up"
-	if Input.is_action_just_pressed("left"):
-		move_dir = "left"
-	if Input.is_action_just_pressed("right"):
-		move_dir = "right"
-	
-	if Input.is_action_just_released(move_dir):
-		if Input.is_action_pressed("down"):
+	if swinging_timer.is_stopped():
+		# Get the input direction and handle the movement/deceleration.
+		if Input.is_action_just_pressed("down"):
 			move_dir = "down"
-		elif Input.is_action_pressed("up"):
+		if Input.is_action_just_pressed("up"):
 			move_dir = "up"
-		elif Input.is_action_pressed("left"):
+		if Input.is_action_just_pressed("left"):
 			move_dir = "left"
-		elif Input.is_action_pressed("right"):
+		if Input.is_action_just_pressed("right"):
 			move_dir = "right"
-		else:
-			move_dir = "none"
+		
+		if Input.is_action_just_released(move_dir) or timer_just_stopped:
+			if Input.is_action_pressed("down"):
+				move_dir = "down"
+			elif Input.is_action_pressed("up"):
+				move_dir = "up"
+			elif Input.is_action_pressed("left"):
+				move_dir = "left"
+			elif Input.is_action_pressed("right"):
+				move_dir = "right"
+			else:
+				move_dir = "none"
+			
+	if move_dir != "none":
+		face_dir = move_dir
 	
 	var direction: Vector2
 	match move_dir:
@@ -59,3 +71,9 @@ func _physics_process(delta: float) -> void:
 
 
 	move_and_slide()
+	
+	timer_just_stopped = false
+
+
+func _on_swinging_timer_timeout() -> void:
+	timer_just_stopped = true
