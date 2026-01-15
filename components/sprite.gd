b98dbb8cad3697ec78de80dab4e3fdc904b96858
unerringCaprition <penumbruh@gmail.com>
2026-01-15 15:17:13 -0800

    Flicker for iframes!

---
+++ components/sprite.gd
@@ -0,0 +1,18 @@
+extends AnimatedSprite2D
+
+@export var iframes: Timer
+
+@export var blink_time: int = 2
+
+func _ready() -> void:
+	iframes.connect("timeout", _on_iframes_timeout)
+	if iframes:
+		await iframes.ready
+
+func _process(delta: float) -> void:
+	if not iframes.is_stopped():
+		if (iframes.time_left * 100) as int % blink_time == 0:
+			visible = !visible
+
+func _on_iframes_timeout() -> void:
+	visible = true
